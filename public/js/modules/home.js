6ea81b8701fd3080978fc940c6e48911c5e47890
core2drew <calupesandro@gmail.com>
2018-10-07 20:30:13 +0800

    fix slider

--- public/js/modules/home.js
+++ public/js/modules/home.js
@@ -18,10 +18,35 @@ jQuery(document).ready(function($) {
           $item.append($caption)
           $ActivitySlider.append($item)
         })
+        sliderInit()
       }else {
-        $NoActivity.show();
-        $ActivitySlider.hide();
+        var $noActivity = $('<div/>').attr('id', 'NoActivity')
+        var $message = $('<h3/>').addClass('message').html('No Activities')
+        $noActivity.append($message)
+        $ActivitySlider.append($noActivity);
       }
+    }
+
+    function sliderInit(){
+      $('#ActivitySlider').slick({
+        prevArrow:
+        `<svg version="1.1" id="prevSlide" class="slidenav" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve">
+          <g>
+            <g id="keyboard-arrow-left">
+              <polygon points="247.35,270.3 130.05,153 247.35,35.7 211.65,0 58.65,153 211.65,306 		"/>
+            </g>
+          </g>
+        </svg>`,
+        nextArrow: 
+        `<svg version='1.1' id="nextSlide" class='slidenav' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='306px' height='306px' viewBox='0 0 306 306' xml:space='preserve'>
+          <g>
+            <g id='chevron-right'>
+              <polygon points="94.35,0 58.65,35.7 175.95,153 58.65,270.3 94.35,306 247.35,153 "/>
+            </g>
+          </g>
+        </svg>`
+      });
     }
 
     function getActivities(){
@@ -31,26 +56,6 @@ jQuery(document).ready(function($) {
         success: function(res) {
           //Generate activities
           generateActivities(res.data, $ActivitySlider)
-
-          $('#ActivitySlider').slick({
-            prevArrow:
-            `<svg version="1.1" id="prevSlide" class="slidenav" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                  width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve">
-              <g>
-                <g id="keyboard-arrow-left">
-                  <polygon points="247.35,270.3 130.05,153 247.35,35.7 211.65,0 58.65,153 211.65,306 		"/>
-                </g>
-              </g>
-            </svg>`,
-            nextArrow: 
-            `<svg version='1.1' id="nextSlide" class='slidenav' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='306px' height='306px' viewBox='0 0 306 306' xml:space='preserve'>
-              <g>
-                <g id='chevron-right'>
-                  <polygon points="94.35,0 58.65,35.7 175.95,153 58.65,270.3 94.35,306 247.35,153 "/>
-                </g>
-              </g>
-            </svg>`
-          });
         },
         error: function(err) {
           console.error("Something went wrong");
@@ -60,7 +65,6 @@ jQuery(document).ready(function($) {
     
     function init(){
       getActivities();
-      
     }
     return {
       init: init
