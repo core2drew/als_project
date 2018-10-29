6e807730811a0ff379a05a9a134784c356f69c50
core2drew <calupesandro@gmail.com>
2018-10-30 03:52:27 +0800

    fix for showing deleted exam

--- includes/reports/exam/exams.php
+++ includes/reports/exam/exams.php
@@ -2,7 +2,7 @@
   $go_back_to_subjects = "$_SERVER[PHP_SELF]?page=reports&sub_page=exam&grade_level=$grade_level";
 
   //Display all student account
-  $query = "SELECT id, title FROM exams WHERE subject_id = $subject_id";
+  $query = "SELECT id, title FROM exams WHERE subject_id = $subject_id AND deleted_at IS NULL";
   $result = mysqli_query($conn, $query);
   $count = mysqli_num_rows($result);
 ?>
